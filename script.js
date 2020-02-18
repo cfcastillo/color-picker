685e6885b316028d152b9b0977ccb4ca9a4b2887
Cecilia Castillo <cecilia.castillo@focussi.com>
2020-02-18 09:22:57 -0700

    Capture first lesson color-picker changes (in progress)

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+	initColorPicker();
+}
+
+function initColorPicker(){
+	let colorbox = document.getElementById("color-picker");
+	let rgb = {
+		red: document.getElementById("red"),
+		green: document.getElementById("green"),
+		blue: document.getElementById("blue")
+	}
+	setColorPickerEventListeners(colorbox, rgb);
+}
+
+function setColorPickerEventListeners(colorbox, rgb){
+	rgb.red.addEventListener('change',() => {
+		console.log("Red Value: ", rgb.red.value);
+	})
+
+	rgb.green.addEventListener('change',() => {
+		console.log("Green Value: ", rgb.green.value);
+	})
+
+	rgb.blue.addEventListener('change',() => {
+		console.log("Blue Value: ", rgb.blue.value);
+	})
+}
